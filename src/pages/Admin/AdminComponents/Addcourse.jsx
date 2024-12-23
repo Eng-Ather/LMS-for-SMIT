bcc6b4c60390aaaa5476f69bf3ab38f1920bf608
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-12-23 17:35:04 +0500

    working

--- src/pages/Admin/AdminComponents/Addcourse.jsx
+++ src/pages/Admin/AdminComponents/Addcourse.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Addcourse() {
-  return <div className="text-2xl">Add Course Ki Screen he!</div>;
+  return <div className=" h-screen overflow-y-scroll text-2xl">Add Course Ki Screen he!</div>;
 }
--- src/pages/Admin/AdminComponents/NewAnnoucements.jsx
+++ src/pages/Admin/AdminComponents/NewAnnoucements.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function NewAnnoucements() {
   return (
-    <div className='text-2xl'>
-      Announcement Screen
+    <div className="h-screen overflow-y-scroll text-2xl">
+      <div className="h-96 "></div>
+      <div className="h-96 "></div>
+      <div className="h-96 "></div>
+      <div className="h-96 "></div>
+      <div className="h-96 "></div>
+      <div className="h-96 "></div>
     </div>
-  )
+  );
 }
