3807a57757285a80de0cc79dda37038ee6508f04
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2024-12-23 20:30:28 +0500

    error solved

--- src/pages/Teacher/TeacherScreens.jsx
+++ src/pages/Teacher/TeacherScreens.jsx
@@ -7,6 +7,7 @@ export default function TeacherScreens() {
     <div className="min-h-screen bg-gray-100 flex shadow h-screen">
       {/* Side block */}
       <div className="bg-gray-200 flex flex-start flex-col w-1/5">
+      
         <div className="font-serif text-headingColor h-16 content-center mx-auto text-2xl ">
           STUDENT
         </div>
