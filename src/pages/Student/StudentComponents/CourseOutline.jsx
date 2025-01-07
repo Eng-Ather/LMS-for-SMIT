7daa1d568e85fbb7fde6a6f49f59696cc90ba543
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-01-07 18:13:23 +0500

    set student dashed board outline

--- src/pages/Student/StudentComponents/CourseOutline.jsx
+++ src/pages/Student/StudentComponents/CourseOutline.jsx
@@ -1,109 +1,3 @@
-// import axios from "axios";
-// import React, { useEffect, useState, useContext } from "react";
-// import { AuthContext } from "../../../context/context";
-// import AppRouts from "../../../constant/constant";
-
-// const CourseOutline = () => {
-//   const { user } = useContext(AuthContext);
-//   const [currentUserCourseId, setCurrentUserCourseId] = useState(user);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const [courseTopics, setCourseTopics] = useState([]);
-//   const [coveredTopics, setCoveredTopics] = useState([]);
-
-//   console.log(user);
-  
-//   const id = currentUserCourseId?.courseId;
-//   const courseId = currentUserCourseId?.courseId
-//   const teacherId = currentUserCourseId?.teacherId
-//   const sectionId = currentUserCourseId?.sectionId
-
-
-//   useEffect(() => {
-//     if (!id) return;
-
-//     const fetchCourseContent = async () => {
-//       try {
-//         const courseid = AppRouts.getCourseOutline + id;
-//         const courseOutline = await axios.get(courseid);
-//         console.log(courseOutline.data.course.topics );
-        
-//         setCourseTopics(courseOutline.data.course.topics || []);
-
-//         let updatedTopics = AppRouts.getCoveredTopics+courseId +"/"+ teacherId +"/"+ sectionId;
-//         updatedTopics = await axios.get(updatedTopics)
-//         console.log(updatedTopics.data.data);
-//           let a = updatedTopics.data.data
-        
-//         setCoveredTopics(a || []);
-//         setLoading(false);
-//       } catch (error) {
-//         setError(error.message);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchCourseContent();
-//   }, [id]);
-
-//   if (loading) return <div className="text-center mt-10 text-gray-600">Loading...</div>;
-//   if (error) return <div className="text-center mt-10 text-red-600">Error: {error}</div>;
-
-//   return (
-//     <div className=" w-full px-3 ">
-
-//        {/* Covered topics */}
-//        <div className="my-8 bg-white border-t-4 border-green-300 shadow-lg rounded-lg p-4 md:p-6 ">
-//         <h3 className="font-serif text-subHeadingColor text-2xl md:text-4xl text-center border-b pb-2 border-subHeadingColor mb-6">
-//          Covered Topics
-//         </h3>
-//         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-//           {coveredTopics.length > 0 ? (
-//             coveredTopics.forEach((topic, index) => (
-//               <div
-//                 key={index}
-//                 className="p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition"
-//               >
-//                 <strong> {index + 1}:</strong> {topic.coveredTopic}
-//               </div>
-//             ))
-//           ) : (
-//             <div className=" w-full text-center p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition"
-//             >------ x ------</div>
-//           )}
-//         </div>
-//       </div>
-
-//       {/* Total topics of course */}
-//       <div className="my-8 bg-white border-t-4 border-navbarColor shadow-lg rounded-lg p-4 md:p-6 ">
-//         <h3 className="font-serif text-headingColor text-2xl md:text-4xl text-center border-b pb-2 border-blue-500 mb-6">
-//          Course Outline
-//         </h3>
-//         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-//           {courseTopics.length > 0 ? (
-//             courseTopics.map((topic, index) => (
-//               <div
-//                 key={index}
-//                 className="p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition"
-//               >
-//                 <strong> {index + 1}:</strong> {topic}
-//               </div>
-//             ))
-//           ) :  (
-//             <div className=" w-full text-center p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition"
-//             >------ x ------</div>
-//           )}
-//         </div>
-//       </div>
-
-     
-//     </div>
-//   );
-// };
-
-// export default CourseOutline;
-// ---------------------------------------------------
-
 import axios from "axios";
 import React, { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../../context/context";
@@ -122,6 +16,7 @@ const CourseOutline = () => {
   const id = currentUserCourseId?.courseId;
   const courseId = currentUserCourseId?.courseId;
   const teacherId = currentUserCourseId?.teacherId;
+  const days = currentUserCourseId?.days;
   const sectionId = currentUserCourseId?.sectionId;
 
   useEffect(() => {
@@ -141,7 +36,10 @@ const CourseOutline = () => {
           "/" +
           teacherId +
           "/" +
-          sectionId;
+          sectionId +
+          "/" +
+          days;
+
         updatedTopics = await axios.get(updatedTopics);
         console.log(updatedTopics.data.data);
 
@@ -161,55 +59,55 @@ const CourseOutline = () => {
   if (loading)
     return <div className="text-center mt-10 text-gray-600">Loading...</div>;
   if (error)
-    return (
-      <div className="text-center mt-10 text-red-600">Error: {error}</div>
-    );
+    return <div className="text-center mt-10 text-red-600">Error: {error}</div>;
 
   return (
-    <div className="w-full px-3">
-      {/* Covered Topics */}
-      <div className="my-8 bg-white border-t-4 border-green-300 shadow-lg rounded-lg p-4 md:p-6">
-        <h3 className="font-serif text-subHeadingColor text-2xl md:text-4xl text-center border-b pb-2 border-subHeadingColor mb-6">
-          Covered Topics
-        </h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {coveredTopics.length > 0 ? (
-            coveredTopics.map((topic, index) => (
-              <div
-                key={index}
-                className="p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition"
-              >
-                <strong> {index + 1}:</strong> {topic.coveredTopic}
+    <div className=" h-screen overflow-auto">
+      <div className="w-full px-3">
+        {/* Covered Topics */}
+        <div className="my-8 bg-white border-t-4 border-green-300 shadow-lg rounded-lg p-4 md:p-6">
+          <h3 className="font-serif text-subHeadingColor text-2xl md:text-4xl text-center border-b pb-2 border-subHeadingColor mb-6">
+            Covered Topics
+          </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {coveredTopics.length > 0 ? (
+              coveredTopics.map((topic, index) => (
+                <div
+                  key={index}
+                  className="p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition"
+                >
+                  <strong> {index + 1}:</strong> {topic.coveredTopic}
+                </div>
+              ))
+            ) : (
+              <div className="w-full text-center p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition">
+                ------ x ------
               </div>
-            ))
-          ) : (
-            <div className="w-full text-center p-4 bg-green-100 border border-green-300 rounded-lg shadow hover:bg-green-200 transition">
-              ------ x ------
-            </div>
-          )}
+            )}
+          </div>
         </div>
-      </div>
 
-      {/* Total Topics of Course */}
-      <div className="my-8 bg-white border-t-4 border-navbarColor shadow-lg rounded-lg p-4 md:p-6">
-        <h3 className="font-serif text-headingColor text-2xl md:text-4xl text-center border-b pb-2 border-blue-500 mb-6">
-          Course Outline
-        </h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {courseTopics.length > 0 ? (
-            courseTopics.map((topic, index) => (
-              <div
-                key={index}
-                className="p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition"
-              >
-                <strong> {index + 1}:</strong> {topic}
+        {/* Total Topics of Course */}
+        <div className="my-8 bg-white border-t-4 border-navbarColor shadow-lg rounded-lg p-4 md:p-6">
+          <h3 className="font-serif text-headingColor text-2xl md:text-4xl text-center border-b pb-2 border-blue-500 mb-6">
+            Course Outline
+          </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {courseTopics.length > 0 ? (
+              courseTopics.map((topic, index) => (
+                <div
+                  key={index}
+                  className="p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition"
+                >
+                  <strong> {index + 1}:</strong> {topic}
+                </div>
+              ))
+            ) : (
+              <div className="w-full text-center p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition">
+                ------ x ------
               </div>
-            ))
-          ) : (
-            <div className="w-full text-center p-4 bg-blue-100 border border-blue-300 rounded-lg shadow hover:bg-blue-200 transition">
-              ------ x ------
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
