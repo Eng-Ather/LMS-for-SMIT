591f03b1d4f8e3cae77359e51846dece2ff1d9da
MrArsalAli <159891871+MrArsalAli@users.noreply.github.com>
2025-01-11 18:23:40 +0500

    addTeacher added

--- src/constant/constant.jsx
+++ src/constant/constant.jsx
@@ -1,4 +1,3 @@
-
 // ********************  TO CONNECT WITH ATHER (PRACTICE-PROJECT) DATABASE ****************
 
 // const dev_url = 'http://localhost:4000';
@@ -12,24 +11,27 @@
 //     getCurrentUserProfile : Base_URL + '/api/currentUserInfo',
 // }
 
-
 // *******************************  TO CONNECT WITH SMIT (PROJECT) DATABASE *******************
-const dev_url = 'http://localhost:5000';
+const dev_url = "http://localhost:5000";
 
 // const prod_url = 'https://api.abc.com';
 
- const Base_URL = dev_url;
+const Base_URL = dev_url;
 
- const AppRouts = {
-    signin : Base_URL + '/user/login',
-    signup : Base_URL + '/user/signup',
+const AppRouts = {
+  signin: Base_URL + "/user/login",
+  signup: Base_URL + "/user/signup",
 
-    getCurrentUserProfile : Base_URL + '/user/currentUserInfo',
+  getCurrentUserProfile: Base_URL + "/user/currentUserInfo",
 
-    getCourseOutline : Base_URL + '/course/course-outline/',
+  getCourseOutline: Base_URL + "/course/course-outline/",
 
-    updateCourseOutline : Base_URL + '/course/cover-topics',
+  updateCourseOutline: Base_URL + "/course/cover-topics",
 
-    getCoveredTopics :Base_URL + '/course/get-cover-topics/'
-}
+  getCoveredTopics: Base_URL + "/course/get-cover-topics/",
+
+  addTeacher: Base_URL + "/admin/addTeacher",
+
+  getAllTeacher: Base_URL + "/admin/getallteachers",
+};
 export default AppRouts;
