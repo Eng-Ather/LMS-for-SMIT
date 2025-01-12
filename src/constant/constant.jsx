82a1043729a05db8a53722a5861d01696134699d
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-01-12 23:45:48 +0500

    fixed error

--- src/constant/constant.jsx
+++ src/constant/constant.jsx
@@ -12,20 +12,39 @@
 // }
 
 // *******************************  TO CONNECT WITH SMIT (PROJECT) DATABASE *******************
+
 const dev_url = "http://localhost:5000";
 
-// const prod_url = 'https://api.abc.com';
+// // const prod_url = 'https://api.abc.com';
 
 const Base_URL = dev_url;
 
 const AppRouts = {
   signin: Base_URL + "/user/login",
+
   signup: Base_URL + "/user/signup",
+
   getCurrentUserProfile: Base_URL + "/user/currentUserInfo",
+
   getCourseOutline: Base_URL + "/course/course-outline/",
+
   updateCourseOutline: Base_URL + "/course/cover-topics",
+
   getCoveredTopics: Base_URL + "/course/get-cover-topics/",
+
   addTeacher: Base_URL + "/admin/addTeacher",
+
   getAllTeacher: Base_URL + "/admin/getallteachers",
+
+  createAssignment: Base_URL + "/assignment/create-assignment",
+
+  submitAssignment: Base_URL + "/assignment/submit-assignment",
+
+  getSpecificCourseAssignment: Base_URL + "/assignment/get-assignment/",
+
+  getStudentSubmitedAssigment:
+    Base_URL + "/assignment/get-submited-assignment/",
+
+  getLeatestAssignment: Base_URL + "/assignment/get-leatest-assignment/",
 };
 export default AppRouts;
