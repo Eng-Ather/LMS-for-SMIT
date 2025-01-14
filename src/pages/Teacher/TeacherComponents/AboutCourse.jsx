ee66ebf9eb3fae4260da88ce9ac77550d9fb690e
jawed shiekh <abduljawed6663@gmail.com>
2025-01-14 20:55:14 +0500

    hello

--- src/pages/Teacher/TeacherComponents/AboutCourse.jsx
+++ src/pages/Teacher/TeacherComponents/AboutCourse.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AboutCourse() {
-  return <div className="text-2xl">About Course Screen</div>;
+  return <div className="text-2xl">About Course </div>;
 }
